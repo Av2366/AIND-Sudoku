245d84678167ea49b42d5109e02e19b903855a7a
Av2366 <arun.venkatadri@lyft.com>
2017-02-24 08:48:13 -0800

    filled out helper functions

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-import re
+
 
 assignments = []
 
@@ -35,12 +35,12 @@ def naked_twins(values):
 
     for box in naked_twin_values:
         both_values= values[box]
-        digit1 = bothvalues[1]
-        digit2 = bothvalues[2]
+        digit1 = both_values[0]
+        digit2 = both_values[1]
         for peer in peers[box]:
             if both_values==values[peer]:
                 for peer in peers[box]:
-                    if: bothvalues !=values[peer]
+                    if both_values !=values[peer]:
                         values[peer] = values[peer].replace(digit1, '')
                         values[peer] = values[peer].replace(digit2, '')
                     else:
@@ -61,10 +61,20 @@ def naked_twins(values):
 
 
 def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    return [s+t for s in a for t in b]
+
+boxes = cross(rows, cols)
 
 def grid_values(grid):
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
     """
     Convert grid into a dict of {square: char} with '123456789' for empties.
     Args:
@@ -74,27 +84,68 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    
 
 def display(values):
-    """
-    Display the values as a 2-D grid.
-    Args:
-        values(dict): The sudoku in dictionary form
-    """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        # Your code here: Use the Eliminate Strategy
+        vaules= only_choice(values)
+        # Your code here: Use the Only Choice Strategy
+         
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, try all possible values."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
